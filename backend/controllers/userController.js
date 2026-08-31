abfdd1570553116487932228c11bc5f6ac567292
thishandhananjaya <pinkgrapes20021128@gmail.com>
2026-08-31 10:53:52 +0530

    Refactor authentication middleware to include user retrieval and access control; improve error handling for user not found and blocked status

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -181,8 +181,7 @@ export const loginUser = (req, res) => {
 
             const token = jwt.sign(
                 {
-                    userId: user._id,
-                    role: user.role
+                    userId: user._id
                 },
                 process.env.JWT_SECRET,
                 {
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-
+import User from "../models/userModel.js";
 export function  authMiddleware(req,res,next){
     const authHeader = req.headers.authorization;
     if(!authHeader){
@@ -10,8 +10,23 @@ export function  authMiddleware(req,res,next){
 
     try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
+       
+        User.findById(decoded.userId)
+            .then((user) => {
+            if(!user){
+                return res.status(401).json({message: "User not found"});
+
+            }
+            if(user.isBlocked){
+                return res.status(403).json({message: "Access Denied"})
+            }
+            req.user = user;
+            next();
+
+        }).catch((err)=> {
+            return res.status(500).json({message: "server not responding" });
+        });
+        
     }catch(err){
         return res.status(401).json({message:"Invalid or Expired Token"});
     }
